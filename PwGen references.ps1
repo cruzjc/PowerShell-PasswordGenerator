1dcb1e88b02a581272266efb989ea7f19021f748
Jeanclyde Cruz <JeanclydeCruz@gmail.com>
2022-07-13 20:46:44 -1000

    Uses arguments instead of fixed number

--- PwGen references.ps1
+++ PwGen references.ps1
@@ -6,3 +6,4 @@
 #for (($i=0);$i -lt $PasswordLength;i++){
 #    $RandomCharacter=Get-Random -Minimum 33 -Maximum 126
 #}
+
--- PwGen.ps1
+++ PwGen.ps1
@@ -1,8 +1,9 @@
-﻿$PasswordLength=16
+﻿$PasswordLength=$args[0]
 $Result=""
+
 1..$PasswordLength | ForEach-Object{
     $RandomCharacter=([char]'a'..[char]'z'+[char]'A'..[char]'Z'+[char]'0'..[char]'9')|Get-Random
     $Result+=[char]$RandomCharacter
 }
 
-Write-Output("Password is ",$Result)
+Write-Output($Result)
