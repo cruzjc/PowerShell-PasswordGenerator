27e904980c1783e12f5bf391b0dd11db11746ead
Jeanclyde Cruz <JeanclydeCruz@gmail.com>
2022-07-13 20:47:04 -1000

    Added useful link

--- PwGen references.ps1
+++ PwGen references.ps1
@@ -2,6 +2,8 @@
 #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-random?view=powershell-7.2
 #https://ss64.com/ascii.html
 
+#https://www.red-gate.com/simple-talk/sysadmin/powershell/how-to-use-parameters-in-powershell/
+
 #original idea
 #for (($i=0);$i -lt $PasswordLength;i++){
 #    $RandomCharacter=Get-Random -Minimum 33 -Maximum 126
