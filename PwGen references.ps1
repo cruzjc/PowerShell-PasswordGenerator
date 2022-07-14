2f902b0fcb58ca379f9807cf53bc05e179e68b75
Jeanclyde Cruz <JeanclydeCruz@gmail.com>
2022-07-13 20:15:43 -1000

    Script commit

---
+++ PwGen references.ps1
@@ -0,0 +1,8 @@
+﻿#usefull
+#https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-random?view=powershell-7.2
+#https://ss64.com/ascii.html
+
+#original idea
+#for (($i=0);$i -lt $PasswordLength;i++){
+#    $RandomCharacter=Get-Random -Minimum 33 -Maximum 126
+#}
---
+++ PwGen.ps1
@@ -0,0 +1,8 @@
+﻿$PasswordLength=16
+$Result=""
+1..$PasswordLength | ForEach-Object{
+    $RandomCharacter=([char]'a'..[char]'z'+[char]'A'..[char]'Z'+[char]'0'..[char]'9')|Get-Random
+    $Result+=[char]$RandomCharacter
+}
+
+Write-Output("Password is ",$Result)
