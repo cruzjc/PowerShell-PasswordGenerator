bee926714bbf58d461c3efa738bf36f1a412a379
Jeanclyde Cruz <JeanclydeCruz@gmail.com>
2022-07-14 20:28:12 -1000

    Encapsulated code in a function; Added Argument check

--- PwGen.ps1
+++ PwGen.ps1
@@ -1,9 +1,27 @@
-﻿$PasswordLength=$args[0]
-$Result=""
+﻿$PasswordLength=0
 
-1..$PasswordLength | ForEach-Object{
-    $RandomCharacter=([char]'a'..[char]'z'+[char]'A'..[char]'Z'+[char]'0'..[char]'9')|Get-Random
-    $Result+=[char]$RandomCharacter
+#argument check
+if(!$args)
+{
+    $PasswordLength=16
+    }
+else
+{
+    $PasswordLength=$args[0]
 }
 
-Write-Output($Result)
+
+function Generate-Password
+{
+    param([int] $Length)
+    
+    $Result=""
+    1..$Length | ForEach-Object{
+    $RandomCharacter=([char]'a'..[char]'z'+[char]'A'..[char]'Z'+[char]'0'..[char]'9')|Get-Random
+    $Result+=[char]$RandomCharacter 
+    }|Out-Null
+    return $Result
+}
+
+
+Write-Output(Generate-Password $PasswordLength)
